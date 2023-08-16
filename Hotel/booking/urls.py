536191352e519c88825a783e557a7e49719f4ff0
Ashish <akrsharma766@gmail.com>
2023-08-16 10:29:11 +0530

    accepted changes

--- Hotel/booking/urls.py
+++ Hotel/booking/urls.py
@@ -5,13 +5,5 @@ from django.urls import path
 app_name = 'Booking'
 
 urlpatterns = [
-<<<<<<< HEAD
-    # Add your other URL patterns here...
-    path('booking/<str:room_type>/<int:room_number>/', views.booking, name='booking_form'),
-    path('showRooms/', views.showRooms, name='showRooms'),
-    # ...
-]
-=======
     path('book_room/', bookRoom, name='login'),
 ]
->>>>>>> a56cc987c97102de83310454f5252a4e30e46011
