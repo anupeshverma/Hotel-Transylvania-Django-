c96970331b0aadc28ee040ba4864245f9d48cd83
Ashish <akrsharma766@gmail.com>
2023-08-16 19:36:19 +0530

    added the add room page and models

--- Hotel/Rooms/urls.py
+++ Hotel/Rooms/urls.py
@@ -6,5 +6,5 @@ app_name = 'Rooms'
 
 urlpatterns = [
     path('showAllRooms/', showAllRooms, name='show_all_rooms'),
-   
+    path('addRoom/', views.addRoom.as_view(), name='add_room'),
 ]
--- Hotel/Rooms/views.py
+++ Hotel/Rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 # Create your views here.
 
@@ -8,3 +10,21 @@ def showAllRooms(request):
     print(rooms)
     
     return render(request, 'show_all_rooms.html', {'rooms':rooms})
+
+class addRoom(LoginRequiredMixin, View): 
+    def get(self, request):
+        return render(request, 'add_room.html')
+    
+    def post(self, request):
+        roomNo = request.POST['roomNo']
+        roomType = request.POST['roomType']
+        capacity = request.POST['roomCapacity']
+        price = request.POST['price']
+        roomImage = request.FILES.get("roomImage")
+        description = request.POST['description']
+        
+        room = Room(roomNo=roomNo, roomType=roomType, capacity=capacity, price=price, roomImage=roomImage, description=description)
+
+        room.save()
+        
+        return render(request, 'show_all_rooms.html')
