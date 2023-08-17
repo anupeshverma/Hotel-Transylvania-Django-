18cdce124eae730f28a776346f88a7e66630e6a0
Ashish <akrsharma766@gmail.com>
2023-08-17 11:04:42 +0530

    added function to check availavility of room based on checkin and checkout type and room category

--- Hotel/Rooms/urls.py
+++ Hotel/Rooms/urls.py
@@ -6,5 +6,6 @@ app_name = 'Rooms'
 
 urlpatterns = [
     path('showAllRooms/', showAllRooms, name='show_all_rooms'),
+    path('showFilteredRooms/', showFilteredRooms, name='show_filtered_rooms'),
     path('addRoom/', views.addRoom.as_view(), name='add_room'),
 ]
