f879c6fa7b69a7603b9bc679eec2decacab16228
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-18 16:36:37 +0530

    update gamil id and passsword

--- Hotel/Hotel/settings.py
+++ Hotel/Hotel/settings.py
@@ -46,8 +46,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "hoteltransylvania46@gmail.com"
-EMAIL_HOST_PASSWORD = "Hotel@TV"
+EMAIL_HOST_USER = "hoteltransylvania42k23@gmail.com"
+EMAIL_HOST_PASSWORD = "Hotel@TV2k23"
 
 
 MIDDLEWARE = [
